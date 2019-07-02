7868a0252ac58a08951d3cbfaf5eb34d34150a87
PasseRR <xie__hai@sina.com>
2019-07-02 15:21:37 +0800

    #12

---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/page1/Solution12Spec.groovy
@@ -0,0 +1,28 @@
+package com.gitee.passerr.leetcode.problem.page1
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution12}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/07/02 15:12
+ * @author xiehai
+ */
+class Solution12Spec extends Specification {
+    Solution12 solution12 = new Solution12()
+
+    @Unroll
+    def "intToRoman(#input) = #result"() {
+        expect:
+        result == this.solution12.intToRoman(input)
+
+        where:
+        input || result
+        1000  || "M"
+        3     || "III"
+        4     || "IV"
+        58    || "LVIII"
+        1994  || "MCMXCIV"
+    }
+}
