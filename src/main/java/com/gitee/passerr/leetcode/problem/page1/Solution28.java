5781df26cff91acd0d00d7b4ffb90942516f7601
PasseRR <xie__hai@sina.com>
2019-07-04 15:16:38 +0800

    #28

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/page1/Solution28.java
@@ -0,0 +1,39 @@
+package com.gitee.passerr.leetcode.problem.page1;
+
+/**
+ * 实现 strStr() 函数。
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ * <p>
+ * 示例 1:
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * <p>
+ * 示例 2:
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ * <p>
+ * 说明:
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+ * @author xiehai
+ * @date 2019/07/04 15:06
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class Solution28 {
+    public int strStr(String haystack, String needle) {
+        // needle为空字符串直接返回
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        int haystackLength = haystack.length();
+        int needleLength = needle.length();
+
+        for (int i = 0; i <= haystackLength - needleLength; i++) {
+            if (haystack.substring(i, i + needleLength).equals(needle)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/page1/Solution28Spec.groovy
@@ -0,0 +1,27 @@
+package com.gitee.passerr.leetcode.problem.page1
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution28}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/07/04 15:11
+ * @author xiehai
+ */
+class Solution28Spec extends Specification {
+    Solution28 solution28 = new Solution28()
+
+    @Unroll
+    def "strStr(#haystack, #needle) = #result"() {
+        expect:
+        result == this.solution28.strStr(haystack, needle)
+
+        where:
+        haystack | needle || result
+        "hello"  | "ll"   || 2
+        "aaaaa"  | "ab"   || -1
+        "a"      | "a"    || 0
+        "aaab"   | "ab"   || 2
+    }
+}
