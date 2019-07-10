540d7fcf0b0e1a43624a8f8f9f8fadc68bb92a71
PasseRR <xie__hai@sina.com>
2019-07-10 15:11:11 +0800

    #44

---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/page1/Solution44Spec.groovy
@@ -0,0 +1,33 @@
+package com.gitee.passerr.leetcode.problem.page1
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution44}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/07/10 11:28
+ * @author xiehai
+ */
+class Solution44Spec extends Specification {
+    Solution44 solution44 = new Solution44()
+
+    @Unroll
+    def "isMatch(#s, #p) = #result"() {
+        expect:
+        result == this.solution44.isMatch(s, p)
+
+        where:
+        s                                                     | p                        || result
+        "aaa"                                                 | "*a"                    || true
+        ""                                                    | "*"                      || true
+        ""                                                    | ""                       || true
+        "abc"                                                 | ""                       || false
+        ""                                                    | "aa"                     || false
+        "abc"                                                 | "*"                      || true
+        "abc"                                                 | "*?*"                    || true
+        "adceb"                                               | "*a*b"                   || true
+        "acdcb"                                               | "a*c?b"                  || false
+        "babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab" | "***bba**a*bbba**aab**b" || false
+    }
+}
