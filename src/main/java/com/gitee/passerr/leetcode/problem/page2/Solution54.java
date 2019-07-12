7566304eb94c396c84885c19b94e4789b189bd73
PasseRR <xie__hai@sina.com>
2019-07-12 14:31:09 +0800

    #54

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/page2/Solution54.java
@@ -0,0 +1,69 @@
+package com.gitee.passerr.leetcode.problem.page2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * [
+ * [ 1, 2, 3 ],
+ * [ 4, 5, 6 ],
+ * [ 7, 8, 9 ]
+ * ]
+ * 输出: [1,2,3,6,9,8,7,4,5]
+ * 示例 2:
+ * <p>
+ * 输入:
+ * [
+ * [1, 2, 3, 4],
+ * [5, 6, 7, 8],
+ * [9,10,11,12]
+ * ]
+ * 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+ * @author xiehai
+ * @date 2019/07/12 13:03
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class Solution54 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int rows = matrix.length;
+        // 空数组处理
+        if (rows == 0 || matrix[0].length == 0) {
+            return result;
+        }
+        int columns = matrix[0].length;
+
+        // 圆圈个数 即循环次数 行列中取最小值
+        int min = Math.min(rows, columns);
+        int circles = min / 2 + min % 2;
+        for (int i = 0; i < circles; i++) {
+            // 上行顺序
+            for (int j = i; j < columns - i; j++) {
+                result.add(matrix[i][j]);
+            }
+            // 右列顺序
+            for (int j = i + 1; j < rows - i; j++) {
+                result.add(matrix[j][columns - i - 1]);
+            }
+            // 如果是奇数行 最内层只有一行或一列
+            if (i == circles - 1 && min % 2 == 1) {
+                break;
+            }
+            // 下行逆序
+            for (int j = columns - i - 2; j >= i; j--) {
+                result.add(matrix[rows - i - 1][j]);
+            }
+            // 左列逆序
+            for (int j = rows - i - 2; j > i; j--) {
+                result.add(matrix[j][i]);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/page2/Solution54Spec.groovy
@@ -0,0 +1,63 @@
+package com.gitee.passerr.leetcode.problem.page2
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution54}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/07/12 13:35
+ * @author xiehai
+ */
+class Solution54Spec extends Specification {
+    Solution54 solution54 = new Solution54()
+
+    @Unroll
+    def "spiralOrder(#matrix) = #result"() {
+        expect:
+        result == this.solution54.spiralOrder(matrix)
+
+        where:
+        matrix        || result
+        [
+            [2, 3, 4],
+            [5, 6, 7],
+            [8, 9, 10],
+            [11, 12, 13],
+            [14, 15, 16]
+        ] as int[][]  || [2, 3, 4, 7, 10, 13, 16, 15, 14, 11, 8, 5, 6, 9, 12]
+        [
+            [1, 2],
+            [3, 4]
+        ] as int[][]  || [1, 2, 4, 3]
+        [
+            [1], [2], [3],
+            [4], [5], [6],
+            [7], [8], [9],
+            [10]
+        ] as int[][]  || [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        [] as int[][] || []
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ] as int[][]  || [1, 2, 3, 6, 9, 8, 7, 4, 5]
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12]
+        ] as int[][]  || [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ] as int[][]  || [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+        [
+            [1, 2, 3, 4, 5],
+            [5, 6, 7, 8, 6],
+            [9, 10, 11, 12, 7],
+            [13, 14, 15, 16, 8]
+        ] as int[][]  || [1, 2, 3, 4, 5, 6, 7, 8, 16, 15, 14, 13, 9, 5, 6, 7, 8, 12, 11, 10]
+    }
+}
