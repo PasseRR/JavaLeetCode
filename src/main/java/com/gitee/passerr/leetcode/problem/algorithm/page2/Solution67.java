90ffc77936d47b4ccf95bbe4e028253bb64ed5e0
PasseRR <xie__hai@sina.com>
2019-07-16 13:52:40 +0800

    #67

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page2/Solution67.java
@@ -0,0 +1,37 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page2;
+
+/**
+ * 给定两个二进制字符串，返回他们的和（用二进制表示）。
+ * 输入为非空字符串且只包含数字 1 和 0。
+ * <p>
+ * 示例 1:
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * <p>
+ * 示例 2:
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ * @author xiehai
+ * @date 2019/07/16 13:37
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class Solution67 {
+    public String addBinary(String a, String b) {
+        int al = a.length(), bl = b.length(), ai = al - 1, bi = bl - 1, carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (ai >= 0 || bi >= 0) {
+            int sum = (ai < 0 ? 0 : Integer.valueOf(a.substring(ai, ai + 1)))
+                + (bi < 0 ? 0 : Integer.valueOf(b.substring(bi, bi + 1))) + carry;
+            sb.append(sum % 2);
+            carry = sum / 2;
+            ai--;
+            bi--;
+        }
+
+        if (carry > 0) {
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/algorithm/page1/Solution67Spec.groovy
@@ -0,0 +1,29 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page1
+
+import com.gitee.passerr.leetcode.problem.algorithm.page2.Solution67
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution67}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/07/16 13:42
+ * @author xiehai
+ */
+class Solution67Spec extends Specification {
+    Solution67 solution67 = new Solution67()
+
+    @Unroll
+    def "addBinary(#a, #b) = #result"() {
+        expect:
+        result == this.solution67.addBinary(a, b)
+
+        where:
+        a      | b      || result
+        ""     | "1"    || "1"
+        "1"    | ""     || "1"
+        "0"    | "1"    || "1"
+        "11"   | "1"    || "100"
+        "1010" | "1011" || "10101"
+    }
+}
