3215eb44d1ff5ab67609c102139a42d7f87a37b9
PasseRR <xie__hai@sina.com>
2019-07-19 17:56:19 +0800

    #83

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page2/Solution83.java
@@ -0,0 +1,40 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page2;
+
+import com.gitee.passerr.leetcode.problem.algorithm.ListNode;
+
+/**
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ * <p>
+ * 示例 1:
+ * 输入: 1->1->2
+ * 输出: 1->2
+ * <p>
+ * 示例 2:
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ * @author xiehai
+ * @date 2019/07/19 17:45
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class Solution83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode node = new ListNode(0);
+        ListNode cursor = node;
+        // 初始化为头结点的值
+        cursor.next = new ListNode(head.val);
+
+        while ((head = head.next) != null) {
+            // 仅当当前节点值不与cursor.next值一样时 增加链表节点
+            if (cursor.next.val != head.val) {
+                cursor = cursor.next;
+                cursor.next = new ListNode(head.val);
+            }
+        }
+
+        return node.next;
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/algorithm/page2/Solution83Spec.groovy
@@ -0,0 +1,29 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page2
+
+import com.gitee.passerr.leetcode.problem.algorithm.ListNode
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution83}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/07/19 17:50
+ * @author xiehai
+ */
+class Solution83Spec extends Specification {
+    Solution83 solution83 = new Solution83()
+
+    @Unroll
+    def "deleteDuplicates(#head) = #result"() {
+        expect:
+        result == this.solution83.deleteDuplicates(head)
+
+        where:
+        head                       || result
+        null                       || null
+        ListNode.of(1)             || ListNode.of(1)
+        ListNode.of(1, 1, 2)       || ListNode.of(1, 2)
+        ListNode.of(1, 2)          || ListNode.of(1, 2)
+        ListNode.of(1, 1, 2, 3, 3) || ListNode.of(1, 2, 3)
+    }
+}
