3fd144de21744e1045e90b928438aa169dd17413
PasseRR <xie__hai@sina.com>
2019-07-22 16:37:38 +0800

    #86

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page2/Solution86.java
@@ -0,0 +1,44 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page2;
+
+import com.gitee.passerr.leetcode.problem.algorithm.ListNode;
+
+/**
+ * 给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+ * 你应当保留两个分区中每个节点的初始相对位置。
+ * <p>
+ * 示例:
+ * 输入: head = 1->4->3->2->5->2, x = 3
+ * 输出: 1->2->2->4->3->5
+ * @author xiehai
+ * @date 2019/07/22 16:29
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class Solution86 {
+    public ListNode partition(ListNode head, int x) {
+        // 小于x的头结点
+        ListNode lessThanX = new ListNode(0);
+        // 小于x链表游标
+        ListNode lessThanXCursor = lessThanX;
+        // 大于等于x的头结点
+        ListNode greaterThanX = new ListNode(0);
+        // 大于等于x链表游标
+        ListNode greaterThanXCursor = greaterThanX;
+
+        // 遍历源链表
+        while (head != null) {
+            if (head.val < x) {
+                lessThanXCursor.next = new ListNode(head.val);
+                lessThanXCursor = lessThanXCursor.next;
+            } else {
+                greaterThanXCursor.next = new ListNode(head.val);
+                greaterThanXCursor = greaterThanXCursor.next;
+            }
+            head = head.next;
+        }
+
+        // 拼接链表
+        lessThanXCursor.next = greaterThanX.next;
+
+        return lessThanX.next;
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/algorithm/page2/Solution86Spec.groovy
@@ -0,0 +1,27 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page2
+
+import com.gitee.passerr.leetcode.problem.algorithm.ListNode
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution86}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/07/22 16:35
+ * @author xiehai
+ */
+class Solution86Spec extends Specification {
+    Solution86 solution86 = new Solution86()
+
+    @Unroll
+    def "partition(#head, #x) = #result"() {
+        expect:
+        result == this.solution86.partition(head, x)
+
+        where:
+        head                          | x || result
+        ListNode.of(1, 4, 3, 2, 5, 2) | 3 || ListNode.of(1, 2, 2, 4, 3, 5)
+        ListNode.of(1, 4, 3, 2, 5, 2) | 0 || ListNode.of(1, 4, 3, 2, 5, 2)
+        ListNode.of(1, 4, 3, 2, 5, 2) | 6 || ListNode.of(1, 4, 3, 2, 5, 2)
+    }
+}
