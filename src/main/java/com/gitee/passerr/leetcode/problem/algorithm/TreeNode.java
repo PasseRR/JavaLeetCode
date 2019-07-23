35ebaa77231516cef9f3d8bb9d3479c3ce311f1b
PasseRR <xie__hai@sina.com>
2019-07-23 12:10:44 +0800

    TreeNode

--- src/main/java/com/gitee/passerr/leetcode/problem/algorithm/TreeNode.java
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/TreeNode.java
@@ -1,5 +1,9 @@
 package com.gitee.passerr.leetcode.problem.algorithm;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Objects;
+
 /**
  * 二叉树节点
  * @author xiehai
@@ -19,5 +23,48 @@ public class TreeNode {
         this(val);
         this.left = left;
         this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TreeNode treeNode = (TreeNode) o;
+        return val == treeNode.val &&
+            Objects.equals(left, treeNode.left) &&
+            Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, left, right);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.addLast(this);
+        while (!deque.isEmpty()) {
+            TreeNode node = deque.poll();
+            if (node != null) {
+                sb.append(node.val).append(",");
+                if (node.left != null || node.right != null) {
+                    deque.offer(node.left);
+                    deque.offer(node.right);
+                }
+            } else {
+                sb.append("null").append(",");
+            }
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append("]");
+
+        return sb.toString();
     }
 }
