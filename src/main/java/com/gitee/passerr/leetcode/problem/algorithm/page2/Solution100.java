b4d8fdaa9491390ee1326586c7636dde89412c15
PasseRR <xie__hai@sina.com>
2019-07-24 16:57:39 +0800

    #100

--- src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page2/Solution100.java
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page2/Solution100.java
@@ -5,29 +5,29 @@ import com.gitee.passerr.leetcode.problem.algorithm.TreeNode;
 /**
  * 给定两个二叉树，编写一个函数来检验它们是否相同。
  * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
- *
+ * <p>
  * 示例 1:
  * 输入:       1         1
- *           / \       / \
- *          2   3     2   3
- *
- *         [1,2,3],   [1,2,3]
+ * / \       / \
+ * 2   3     2   3
+ * <p>
+ * [1,2,3],   [1,2,3]
  * 输出: true
- *
+ * <p>
  * 示例 2:
  * 输入:      1          1
- *           /           \
- *          2             2
- *
- *         [1,2],     [1,null,2]
+ * /           \
+ * 2             2
+ * <p>
+ * [1,2],     [1,null,2]
  * 输出: false
- *
+ * <p>
  * 示例 3:
  * 输入:       1         1
- *           / \       / \
- *          2   1     1   2
- *
- *         [1,2,1],   [1,1,2]
+ * / \       / \
+ * 2   1     1   2
+ * <p>
+ * [1,2,1],   [1,1,2]
  * 输出: false
  * @author xiehai
  * @date 2019/07/23 11:19
@@ -35,10 +35,12 @@ import com.gitee.passerr.leetcode.problem.algorithm.TreeNode;
  */
 public class Solution100 {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null || q == null) {
-            return p == q;
-        } else {
-            return p.val == q.val && this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
-        }
+        return
+            // 其中一个节点为null
+            p == null || q == null
+                // 判断是否都为null
+                ? p == q
+                // 比较当前节点值及递归比较左右节点是否一致
+                : p.val == q.val && this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
     }
 }
