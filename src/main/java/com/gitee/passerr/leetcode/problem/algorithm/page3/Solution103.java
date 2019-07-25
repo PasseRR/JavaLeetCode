bca1a1370e179d5fd0f05e8a5d585faac9041df2
PasseRR <xie__hai@sina.com>
2019-07-25 17:11:34 +0800

    #103

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution103.java
@@ -0,0 +1,63 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3;
+
+import com.gitee.passerr.leetcode.problem.algorithm.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+/**
+ * 给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回锯齿形层次遍历如下：
+ * <p>
+ * [
+ * [3],
+ * [20,9],
+ * [15,7]
+ * ]
+ * @author xiehai
+ * @date 2019/07/25 16:25
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ * @see Solution102
+ */
+public class Solution103 {
+    @SuppressWarnings("unchecked")
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        BiConsumer<TreeNode, Integer> recursion = new BiConsumer<TreeNode, Integer>() {
+            @Override
+            public void accept(TreeNode node, Integer layer) {
+                if (null == node) {
+                    return;
+                }
+                if (result.size() < layer + 1) {
+                    result.add(new LinkedList<>());
+                }
+                // 将数字加入对应层级
+                // 根据层级奇偶性进行锯齿形变换
+                if (layer % 2 == 0) {
+                    ((LinkedList) result.get(layer)).addLast(node.val);
+                } else {
+                    ((LinkedList) result.get(layer)).addFirst(node.val);
+                }
+                // 遍历左树
+                this.accept(node.left, layer + 1);
+                // 遍历右樹
+                this.accept(node.right, layer + 1);
+            }
+        };
+        recursion.accept(root, 0);
+
+        return result;
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution103Spec.groovy
@@ -0,0 +1,31 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3
+
+import com.gitee.passerr.leetcode.problem.algorithm.TreeNode
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution103}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/07/25 16:26
+ * @author xiehai
+ */
+class Solution103Spec extends Specification {
+    Solution103 solution103 = new Solution103()
+
+    @Unroll
+    def "zigzagLevelOrder(#root) = #result"() {
+        expect:
+        result == this.solution103.zigzagLevelOrder(root)
+
+        where:
+        root                                                                                  || result
+        new TreeNode(2, new TreeNode(1), new TreeNode(3))                                     || [[2], [3, 1]]
+        new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7))) || [[3], [20, 9], [15, 7]]
+        new TreeNode(
+            1,
+            new TreeNode(2, new TreeNode(4), null),
+            new TreeNode(3, null, new TreeNode(5))
+        )                                                                                     || [[1], [3, 2], [4, 5]]
+    }
+}
