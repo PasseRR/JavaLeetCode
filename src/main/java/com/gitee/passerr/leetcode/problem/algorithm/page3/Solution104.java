d815ad82e318ac301e2d70eeec5ca4950ad1ce11
PasseRR <xie__hai@sina.com>
2019-07-25 17:22:34 +0800

    #104

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution104.java
@@ -0,0 +1,28 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3;
+
+import com.gitee.passerr.leetcode.problem.algorithm.TreeNode;
+
+/**
+ * 给定一个二叉树，找出其最大深度。
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 说明: 叶子节点是指没有子节点的节点。
+ * <p>
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回它的最大深度 3 。
+ * @author xiehai
+ * @date 2019/07/25 17:13
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class Solution104 {
+    public int maxDepth(TreeNode root) {
+        // 递归
+        return root == null ? 0 : 1 + Math.max(this.maxDepth(root.left), this.maxDepth(root.right));
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution104Spec.groovy
@@ -0,0 +1,33 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3
+
+import com.gitee.passerr.leetcode.problem.algorithm.TreeNode
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution104}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/07/25 17:18
+ * @author xiehai
+ */
+class Solution104Spec extends Specification {
+    Solution104 solution104 = new Solution104()
+
+    @Unroll
+    def "maxDepth(#root) = #result"() {
+        expect:
+        result == this.solution104.maxDepth(root)
+
+        where:
+        root                                   || result
+        null                                   || 0
+        new TreeNode(1)                        || 1
+        new TreeNode(1, new TreeNode(1), null) || 2
+        new TreeNode(1, null, new TreeNode(1)) || 2
+        new TreeNode(
+            1,
+            new TreeNode(2, new TreeNode(3), null),
+            new TreeNode(4, new TreeNode(5), new TreeNode(6))
+        )                                      || 3
+    }
+}
