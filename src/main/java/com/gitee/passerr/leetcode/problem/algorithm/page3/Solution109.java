ce0fc6218b81d3b491651e45bfb14c72224010f1
PasseRR <xie__hai@sina.com>
2019-07-31 14:29:51 +0800

    #109

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution109.java
@@ -0,0 +1,56 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3;
+
+import com.gitee.passerr.leetcode.problem.algorithm.ListNode;
+import com.gitee.passerr.leetcode.problem.algorithm.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
+/**
+ * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ * <p>
+ * 示例:
+ * 给定的有序链表： [-10, -3, 0, 5, 9],
+ * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+ * <p>
+ * 0
+ * / \
+ * -3   9
+ * /   /
+ * -10  5
+ * @author xiehai
+ * @date 2019/07/31 14:03
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ * @see Solution108
+ */
+public class Solution109 {
+    public TreeNode sortedListToBST(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        // 链表转数组
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+
+        BiFunction<Integer, Integer, TreeNode> recursion = new BiFunction<Integer, Integer, TreeNode>() {
+            @Override
+            public TreeNode apply(Integer begin, Integer end) {
+                if (begin > end) {
+                    return null;
+                }
+                // 取中间节点为根节点
+                int mid = (begin + end) / 2;
+                TreeNode node = new TreeNode(list.get(mid));
+                // 递归生成左树
+                node.left = this.apply(begin, mid - 1);
+                // 递归生成右树
+                node.right = this.apply(mid + 1, end);
+                return node;
+            }
+        };
+
+        return list.size() == 0 ? null : recursion.apply(0, list.size() - 1);
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution109Spec.groovy
@@ -0,0 +1,30 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3
+
+import com.gitee.passerr.leetcode.problem.algorithm.ListNode
+import com.gitee.passerr.leetcode.problem.algorithm.TreeNode
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution109}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/07/31 14:14
+ * @author xiehai
+ */
+class Solution109Spec extends Specification {
+    Solution109 solution109 = new Solution109()
+
+    @Unroll
+    def "sortedListToBST(#head) = #result"() {
+        expect:
+        result == this.solution109.sortedListToBST(head)
+
+        where:
+        head                          || result
+        ListNode.of(-10, -3, 0, 5, 9) || new TreeNode(
+            0,
+            new TreeNode(-10, null, new TreeNode(-3)),
+            new TreeNode(5, null, new TreeNode(9))
+        )
+    }
+}
