ed953d32279fee6db7d248801a498a0db9fd28fd
PasseRR <xie__hai@sina.com>
2019-08-05 11:34:44 +0800

    #114

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution114.java
@@ -0,0 +1,57 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3;
+
+import com.gitee.passerr.leetcode.problem.algorithm.TreeNode;
+
+/**
+ * 给定一个二叉树，原地将它展开为链表。
+ * 例如，给定二叉树
+ * <p>
+ * 1
+ * / \
+ * 2   5
+ * / \   \
+ * 3   4   6
+ * <p>
+ * 将其展开为：
+ * 1
+ * \
+ * 2
+ * \
+ * 3
+ * \
+ * 4
+ * \
+ * 5
+ * \
+ * 6
+ * @author xiehai
+ * @date 2019/08/01 09:37
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class Solution114 {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = null;
+        root.right = null;
+        // 递归左树
+        this.flatten(left);
+        // 递归右树
+        this.flatten(right);
+        // 左树直接拼接到右树
+        if (null != left) {
+            root.right = left;
+        }
+        // 若右树存在 拼接在当前右树后面
+        if (null != right) {
+            TreeNode cursor = root;
+            while (cursor.right != null) {
+                cursor = cursor.right;
+            }
+            cursor.right = right;
+        }
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution114Spec.groovy
@@ -0,0 +1,35 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3
+
+import com.gitee.passerr.leetcode.problem.algorithm.TreeNode
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution114}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/08/05 11:28
+ * @author xiehai
+ */
+class Solution114Spec extends Specification {
+    Solution114 solution114 = new Solution114()
+
+    @Unroll
+    def "flatten(#root)"() {
+        when:
+        this.solution114.flatten(root)
+        then:
+        result == root
+
+        where:
+        root || result
+        new TreeNode(
+            1,
+            new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+            new TreeNode(5, null, new TreeNode(6))
+        )    || new TreeNode(
+            1,
+            null,
+            new TreeNode(2, null, new TreeNode(3, null, new TreeNode(4, null, new TreeNode(5, null, new TreeNode(6)))))
+        )
+    }
+}
