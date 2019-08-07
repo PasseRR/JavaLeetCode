01c175cfb2df3e8064f41398fde12455a6dc8839
PasseRR <xie__hai@sina.com>
2019-08-07 09:16:37 +0800

    #145

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution145.java
@@ -0,0 +1,45 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3;
+
+import com.gitee.passerr.leetcode.problem.algorithm.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * 给定一个二叉树，返回它的 后序 遍历。
+ * 示例:
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * 输出: [3,2,1]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * @author xiehai
+ * @date 2019/08/07 09:13
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class Solution145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        // 递归遍历
+        Consumer<TreeNode> postOrder = new Consumer<TreeNode>() {
+            @Override
+            public void accept(TreeNode node) {
+                if(node == null) {
+                    return;
+                }
+                this.accept(node.left);
+                this.accept(node.right);
+                result.add(node.val);
+            }
+        };
+        postOrder.accept(root);
+
+        return result;
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution145Spec.groovy
@@ -0,0 +1,25 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3
+
+import com.gitee.passerr.leetcode.problem.algorithm.TreeNode
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution145}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/08/07 09:14
+ * @author xiehai
+ */
+class Solution145Spec extends Specification {
+    Solution145 solution145 = new Solution145()
+
+    @Unroll
+    def "postorderTraversal(#root) = #result"() {
+        expect:
+        result == this.solution145.postorderTraversal(root)
+
+        where:
+        root                                                          || result
+        new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3))) || [3, 2, 1]
+    }
+}
