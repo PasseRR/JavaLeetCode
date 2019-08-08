c2e06ec9b6ab45fd1e57301f68915c1ffce3e62c
PasseRR <xie__hai@sina.com>
2019-08-08 17:18:47 +0800

    #141

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution141.java
@@ -0,0 +1,45 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3;
+
+import com.gitee.passerr.leetcode.problem.algorithm.ListNode;
+
+/**
+ * 给定一个链表，判断链表中是否有环。
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * <p>
+ * 示例 1：
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * <p>
+ * 示例 2：
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * <p>
+ * 示例 3：
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ * @author xiehai
+ * @date 2019/08/08 16:35
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        // 利用快慢指针 若两指针相同 则有环 否则无环
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/algorithm/page3/Solution141Spec.groovy
@@ -0,0 +1,34 @@
+package com.gitee.passerr.leetcode.problem.algorithm.page3
+
+import com.gitee.passerr.leetcode.problem.algorithm.ListNode
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Solution141}测试用例
+ * @Copyright (c)tellyes tech. inc. co.,ltd
+ * @date 2019/08/08 17:05
+ * @author xiehai
+ */
+class Solution141Spec extends Specification {
+    Solution141 solution141 = new Solution141()
+
+    @Unroll
+    def "hasCycle(#head) = #result"() {
+        expect:
+        result == this.solution141.hasCycle(head)
+
+        where:
+        head                           || result
+        ({ it ->
+            ListNode node1 = new ListNode(1)
+            ListNode node2 = new ListNode(2)
+            node1.next = node2
+            ListNode node3 = new ListNode(3)
+            node2.next = node3
+            ListNode node4 = new ListNode(4)
+            node3.next = node4
+            return node1
+        } as Closure<ListNode>).call() || false
+    }
+}
