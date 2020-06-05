035308a40728e233dd33cbb7628c1cdd71c2bf35
PasseRR <xie__hai@sina.com>
2020-06-05 15:01:04 +0800

    程序员面试金典第6版 01.01

---
+++ src/main/java/com/gitee/passerr/leetcode/problem/lcci/page1/Lcci0101.java
@@ -0,0 +1,36 @@
+package com.gitee.passerr.leetcode.problem.lcci.page1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 实现一个算法，确定一个字符串 s 的所有字符是否全都不同。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: s = "leetcode"
+ * 输出: false
+ * 示例 2：
+ * <p>
+ * 输入: s = "abc"
+ * 输出: true
+ * 限制：
+ * <p>
+ * 0 <= len(s) <= 100
+ * 如果你不使用额外的数据结构，会很加分。
+ * @author xiehai
+ * @date 2020/06/05 14:53
+ * @Copyright(c) tellyes tech. inc. co.,ltd
+ */
+public class Lcci0101 {
+    public boolean isUnique(String astr) {
+        Set<Character> set = new HashSet<>();
+        for (char c : astr.toCharArray()) {
+            if (!set.add(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/test/groovy/com/gitee/passerr/leetcode/problem/lcci/page1/Lcci0101Spec.groovy
@@ -0,0 +1,25 @@
+package com.gitee.passerr.leetcode.problem.lcci.page1
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * {@link Lcci0101Spec}单元测试
+ * @date 2020/06/05 14:55
+ * @Copyright (c) wisewe co.,ltd
+ * @author xiehai
+ */
+class Lcci0101Spec extends Specification {
+    Lcci0101 lcci0101 = new Lcci0101()
+
+    @Unroll
+    def "isUnique(#astr) = #result"() {
+        expect:
+        result == this.lcci0101.isUnique(astr)
+
+        where:
+        astr       || result
+        "leetcode" || false
+        "abc"      || true
+    }
+}
