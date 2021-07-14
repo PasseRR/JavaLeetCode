20b7ac5a2d460d778cfbb8f65fd99d70724a74aa
PasseRR <xie__hai@sina.com>
2021-07-14 09:21:42 +0800

    # 177

--- src/main/java/com/gitee/passerr/leetcode/problem/database/page1/177.sql
+++ src/main/java/com/gitee/passerr/leetcode/problem/database/page1/177.sql
@@ -14,6 +14,7 @@
 # +------------------------+
 # | 200                    |
 # +------------------------+
+
 CREATE FUNCTION `getNthHighestSalary`(`N` INT) RETURNS INT
 BEGIN
     SET `N` := `N` - 1;
