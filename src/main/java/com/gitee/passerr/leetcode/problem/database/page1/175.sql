26ec72d03ef3a48cce09c9e4cc5846beed0aa9d6
PasseRR <xie__hai@sina.com>
2021-07-14 09:22:49 +0800

    # 175

--- src/main/java/com/gitee/passerr/leetcode/problem/database/page1/175.sql
+++ src/main/java/com/gitee/passerr/leetcode/problem/database/page1/175.sql
@@ -22,6 +22,7 @@
 #
 # 编写一个 SQL 查询，满足条件：无论 person 是否有地址信息，都需要基于上述两表提供 person 的以下信息：
 # FirstName, LastName, City, State
+
 SELECT `p`.`FirstName`, `p`.`LastName`, `a`.`City`, `a`.`State`
 FROM `Person` `p`
          LEFT JOIN `Address` `a` ON `p`.`PersonId` = `a`.`PersonId`
