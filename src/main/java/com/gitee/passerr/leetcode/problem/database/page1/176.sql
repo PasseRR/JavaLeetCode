b5b71bfc3333335f2e0f5fc27edf9b768e705f84
PasseRR <xie__hai@sina.com>
2021-07-14 09:22:24 +0800

    # 176

--- src/main/java/com/gitee/passerr/leetcode/problem/database/page1/176.sql
+++ src/main/java/com/gitee/passerr/leetcode/problem/database/page1/176.sql
@@ -14,6 +14,7 @@
 # +---------------------+
 # | 200                 |
 # +---------------------+
+
 SELECT MAX(`Salary`) AS `SecondHighestSalary`
 FROM `Employee`
 WHERE `Salary` < (SELECT MAX(`t`.`SALARY`) FROM `Employee` `t`)
